128af6f72db4aaaf61885dda8497ed94a494aa00
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-05-04 17:04:43 +0700

    alive: change string

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -306,22 +306,14 @@ async def amireallyalive(alive):
     user = await bot.get_me()
     uptime = await get_readable_time((time.time() - StartTime))
     output = (
-        f"**♕ ┗┓ {DEFAULTUSER} USERBOT ┏┛ ♕** \n\n"
-        f"\n__**{ALIVE_TEKS_CUSTOM}**__\n"
-        f"┏━━━━━━━━━━━━━━ \n"
-        f"┣ ✥ `Master   : `{DEFAULTUSER} \n"
-        f"┣ ✥ `Username : `@{user.username} \n"
-        f"┣ ✥ `Telethon : `Ver {version.__version__} \n"
-        f"┣ ✥ `Python   : `Ver {python_version()} \n"
-        f"┣ ✥ `Bot Ver  : `{BOT_VER} \n"
-        f"┣ ✥ `Branch   : `{UPSTREAM_REPO_BRANCH} \n"
-        f"┣ ✥ `Modules  : `{len(modules)} Modules \n"
-        f"┣ ✥ `Uptime   : `{uptime} \n"
-        f"┗━━━━━━━━━━━━━━ \n"
-        f"❃ **Repo Userbot :** [Man-Userbot](https://github.com/mrismanaziz/Man-Userbot) \n"
-        f"❃ **Grup Userbot :** [Tekan Disini](https://t.me/sharinguserbot) \n"
-        f"❃ **Owner :** [Risman](t.me/mrismanaziz) \n"
-        f"┗━━━━━━━━━━━━━━ \n"
+        f"**[Man-Userbot](https://github.com/mrismanaziz/Man-Userbot) is Up and Running.**\n\n"
+        f"__**{ALIVE_TEKS_CUSTOM}**__\n\n"
+        f"⚡️ Master : `{DEFAULTUSER} \n"
+        f"⚡️ Modules : `{len(modules)} Modules \n"
+        f"⚡️ Bot Version : `{BOT_VER} \n"
+        f"⚡️ Python Version : `Ver {python_version()} \n"
+        f"⚡️ Telethon Version : `Ver {version.__version__} \n"
+        f"⚡️ Bot Uptime : `{uptime} \n"
     )
     if ALIVE_LOGO:
         try:
