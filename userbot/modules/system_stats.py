4c98c9588f8d62002f88e4d259a5c66f36991bbf
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-05-05 11:48:51 +0700

    alive: change markdown to parse html

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -314,7 +314,8 @@ async def amireallyalive(alive):
         f"⚡️ **Python Version :** `{python_version()}` \n"
         f"⚡️ **Telethon Version :** `{version.__version__}` \n"
         f"⚡️ **Bot Uptime :** `{uptime}` \n\n"
-        "    **[Support](https://t.me/sharinguserbot)** | **[Channel](https://t.me/Lunatic0de)** | **[Owner](t.me/mrismanaziz)**"
+        "    <a href = https://t.me/SharingUserbot><b>Support</b></a> | <a href = https://t.me/Lunatic0de><b>Channel</b></a> | <a href = https://t.me/mrismanaziz><b>Owner</b></a>",
+        parse_mode="html",
     )
     if ALIVE_LOGO:
         try:
