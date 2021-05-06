632f9c965b6c8f71ac3cc045a081055fd20a7984
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-05-07 00:37:40 +0700

    Update vcg.py

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -1,6 +1,8 @@
 # Ported by @vckyou
 
+from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
 from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
 
 from userbot import CMD_HELP
 from userbot.events import register
@@ -17,19 +19,46 @@ def user_list(l, n):
         yield l[i : i + n]
 
 
-@register(outgoing=True, pattern=r"^\.svc (.*)")
-async def _(event):
+@register(outgoing=True, pattern=r"^\.startvc (.*)")
+async def _(e):
     try:
-        await event.client(stopvc(await get_call(event)))
-        await event.edit(event, "`Voice Chat Stopped...`")
+        await e.client(startvc(e.chat_id))
+        await eor(e, "`Voice Chat Started...`")
     except Exception as ex:
-        await event.edit(event, f"`{str(ex)}`")
+        await eor(e, f"`{str(ex)}`")
+
+
+@register(outgoing=True, pattern=r"^\.stopvc (.*)")
+async def _(e):
+    try:
+        await e.client(stopvc(await get_call(e)))
+        await eor(e, "`Voice Chat Stopped...`")
+    except Exception as ex:
+        await eor(e, f"`{str(ex)}`")
+
+
+@register(outgoing=True, pattern=r"^\.vcinvite (.*)")
+async def _(e):
+    ok = await eor(e, "`Inviting Members to Voice Chat...`")
+    users = []
+    z = 0
+    async for x in e.client.iter_participants(e.chat_id):
+        if not x.bot:
+            users.append(x.id)
+    hmm = list(user_list(users, 6))
+    for p in hmm:
+        try:
+            await e.client(invitetovc(call=await get_call(e), users=p))
+            z += 6
+        except BaseException:
+            pass
+    await ok.edit(f"`Invited {z} users`")
 
 
 CMD_HELP.update(
     {
         "vcg": "**Plugin : **`vcg`\
-        \n\n  •  **Syntax :** `.svc`\
+        \n\n  •  **Syntax :** `.stopvc`\
         \n  •  **Function : **Memberhentikan Voice Call Group (membutuhkan Hak akses VCG).\
     "
     }
